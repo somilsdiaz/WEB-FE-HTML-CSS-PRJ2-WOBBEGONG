95d325f0f1a6f6de3a33d07a2312273693e67c54
Valeria Jiménez <83371386+Val-jimenez@users.noreply.github.com>
2024-09-19 16:56:43 -0500

    Update Subheader.tsx

--- src/components/Subheader.tsx
+++ src/components/Subheader.tsx
@@ -2,21 +2,21 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { menuItems } from '../data/DataCategory'; // Asegúrate de que este archivo existe y tiene el formato adecuado.
+import { menuItems } from '../data/DataCategory'; 
 
 const Subheader: React.FC = () => {
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
 
   const toggleCategory = (category: string) => {
     if (activeCategory === category) {
-      setActiveCategory(null); // Cierra el submenú si se hace clic nuevamente
+      setActiveCategory(null); 
     } else {
-      setActiveCategory(category); // Abre el submenú para la categoría seleccionada
+      setActiveCategory(category); 
     }
   };
 
   return (
-    <section className="bg-purple-800 text-white pb-2">
+    <section className="bg-[#5249cc] text-white pb-2">
       <nav className="flex flex-col md:flex-row justify-center items-start md:justify-left md:items-center gap-0 md:gap-5 pl-0 md:pl-5">
         {menuItems.map((item) => (
           <div
@@ -27,19 +27,19 @@ const Subheader: React.FC = () => {
           >
             <button
               onClick={() => toggleCategory(item.category)}
-              className="block w-full text-left hover:bg-purple-600 px-5 py-2 md:px-4 md:py-2 rounded md:rounded-none"
+              className="block w-full text-left hover:bg-[purple-600] px-5 py-2 md:px-4 md:py-2 rounded md:rounded-none"
             >
               {item.category}
             </button>
             <ul
               className={`submenu ${
                 activeCategory === item.category ? 'block' : 'hidden'
-              } md:absolute md:left-0 md:top-full mt-1 md:mt-0 md:bg-purple-700 md:rounded-lg w-full md:w-max md:z-10`}
+              } md:absolute md:left-0 md:top-full mt-1 md:mt-0 md:bg-[#5249cc] md:rounded-lg w-full md:w-max md:z-10`}
             >
               {item.links.map((link) => (
                 <li
                   key={link.name}
-                  className="px-5 py-2 md:px-4 md:py-2 hover:bg-purple-500 w-full md:w-auto"
+                  className="px-5 py-2 md:px-4 md:py-2 hover:bg-[#949de0] w-full md:w-auto"
                 >
                   <Link
                     to={link.url}
@@ -58,3 +58,4 @@ const Subheader: React.FC = () => {
 };
 
 export default Subheader;
+
