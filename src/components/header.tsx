0fa038f5811b3e83c33cfb080f57ca9c4a1e2d41
somilsdiaz <somil.sandoval@icloud.com>
2024-09-19 10:55:58 -0500

    feat(UH2006):styles were migrated using tailwind

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,35 +12,36 @@ type HeaderProps = {
 
 const Header: React.FC<HeaderProps> = ({ cartLink, accountLink }) => {
     return (
-        <header>
-            <div>
+        <header className="bg-[#4C37C0] flex justify-between items-center p-4">
+            <div className="logo rounded-full w-20 h-20 flex items-center justify-center">
                 <a href="/">
-                    <img src={logo} alt="Logo" />
+                    <img src={logo} alt="Logo" className="w-20 h-auto rounded-full border-2 border-[#5c74e0]" />
                 </a>
             </div>
 
-            <div>
+            <div className="center flex items-center justify-center">
                 <input
                     type="text"
                     placeholder="Buscar productos"
-                    className="px-4 py-2 border border-gray-300 rounded-lg"
+                    className="h-[35px] border-none px-4 rounded-l-lg shadow-sm outline-none"
                 />
-                <button type="submit">
-                    <img src={loupe} alt="Buscar" />
+                <button type="submit" className="h-[35px] border-none px-4 bg-white rounded-r-lg flex items-center justify-center shadow-sm cursor-pointer">
+                    <img src={loupe} alt="Buscar" className="w-4 h-4" />
                 </button>
             </div>
 
-            <div>
-                <a href={cartLink}>
-                    <img src={cart} alt="Carrito" />
+            <div className="links flex justify-center items-center gap-5">
+                <a href={cartLink} className="flex flex-col items-center text-white text-sm">
+                    <img src={cart} alt="Carrito" className="w-[35px]" />
                     <span>Carrito</span>
                 </a>
-                <a href={accountLink}>
-                    <img src={user} alt="Mi Cuenta" />
+                <a href={accountLink} className="flex flex-col items-center text-white text-sm">
+                    <img src={user} alt="Mi Cuenta" className="w-[35px]" />
                     <span>Mi Cuenta</span>
                 </a>
             </div>
         </header>
     );
 };
+
 export default Header;
