7cb48137f280c403c514f1d79970906ed65e14b0
Edgar Garcia <aedavila@uninorte.edu.co>
2024-09-20 13:10:04 -0500

    Merge with main branch, up to date to solve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
           </Routes>
           <PromoImage imageId={1}/>
           <PromoImage imageId={2}/>
-          
+          <PLP /> 
           <ProductosDestacados />
 
         </main>
