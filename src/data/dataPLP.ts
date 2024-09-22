eb98219472467149e2359b5c39344322d034e6a8
Edgar Garcia <aedavila@uninorte.edu.co>
2024-09-22 13:53:50 -0500

    Add missing products

--- src/data/dataPLP.ts
+++ src/data/dataPLP.ts
@@ -38,9 +38,46 @@ import img37 from "../assets/plpimg37.png"
 import img38 from "../assets/plpimg38.png"
 import img39 from "../assets/plpimg39.png"
 import img40 from "../assets/plpimg40.png"
-
-
-
+import img41 from "../assets/plpimg41.png"
+import img42 from "../assets/plpimg42.png"
+import img43 from "../assets/plpimg43.png"
+import img44 from "../assets/plpimg44.png"
+import img45 from "../assets/plpimg45.png"
+import img46 from "../assets/plpimg46.png"
+import img47 from "../assets/plpimg47.png"
+import img48 from "../assets/plpimg48.png"
+import img49 from "../assets/plpimg49.png"
+import img50 from "../assets/plpimg50.png"
+import img51 from "../assets/plpimg51.png"
+import img52 from "../assets/plpimg52.png"
+import img53 from "../assets/plpimg53.png"
+import img54 from "../assets/plpimg54.png"
+import img55 from "../assets/plpimg55.png"
+import img56 from "../assets/plpimg56.png"
+import img57 from "../assets/plpimg57.png"
+import img58 from "../assets/plpimg58.png"
+import img59 from "../assets/plpimg59.png"
+import img60 from "../assets/plpimg60.png"
+import img61 from "../assets/plpimg61.png"
+import img62 from "../assets/plpimg62.png"
+import img63 from "../assets/plpimg63.png"
+import img64 from "../assets/plpimg64.png"
+import img65 from "../assets/plpimg65.png"
+import img66 from "../assets/plpimg66.png"
+import img67 from "../assets/plpimg67.png"
+import img68 from "../assets/plpimg68.png"
+import img69 from "../assets/plpimg69.png"
+import img70 from "../assets/plpimg70.png"
+import img71 from "../assets/plpimg71.png"
+import img72 from "../assets/plpimg72.png"
+import img73 from "../assets/plpimg73.png"
+import img74 from "../assets/plpimg74.png"
+import img75 from "../assets/plpimg75.png"
+import img76 from "../assets/plpimg76.png"
+import img77 from "../assets/plpimg77.png"
+import img78 from "../assets/plpimg78.png"
+import img79 from "../assets/plpimg79.png"
+import img80 from "../assets/plpimg80.png"
 
 
 export const products = [
@@ -462,6 +499,428 @@ export const products = [
       img: img40,
       category: "Hogar",
       subcategory: "Baño",
+    },
+    // Video (faltan 4 productos)
+    {
+      id: 41,
+      name: "Panasonic HC-X2000",
+      price: 4500000,
+      discountPrice: 4400000,
+      rating: 4.7,
+      img: img41,
+      category: "Tecnología",
+      subcategory: "Video",
+    },
+    {
+      id: 42,
+      name: "GoPro HERO9",
+      price: 1600000,
+      discountPrice: 1500000,
+      rating: 4.8,
+      img: img42,
+      category: "Tecnología",
+      subcategory: "Video",
+    },
+    {
+      id: 43,
+      name: "Blackmagic Pocket Cinema Camera 6K",
+      price: 7000000,
+      discountPrice: 6900000,
+      rating: 4.9,
+      img: img43,
+      category: "Tecnología",
+      subcategory: "Video",
+    },
+    {
+      id: 44,
+      name: "Canon VIXIA HF G50",
+      price: 2500000,
+      discountPrice: 2400000,
+      rating: 4.7,
+      img: img44,
+      category: "Tecnología",
+      subcategory: "Video",
+    },
+    {
+      id: 45,
+      name: "Sony FDR-AX700",
+      price: 3600000,
+      discountPrice: 3500000,
+      rating: 4.8,
+      img: img45,
+      category: "Tecnología",
+      subcategory: "Video",
+    },
+
+    // Impresión (faltan 5 productos)
+    {
+      id: 46,
+      name: "Brother HL-L2350DW",
+      price: 500000,
+      discountPrice: 480000,
+      rating: 4.6,
+      img: img46,
+      category: "Tecnología",
+      subcategory: "Impresión",
+    },
+    {
+      id: 47,
+      name: "Epson EcoTank L3150",
+      price: 800000,
+      discountPrice: 770000,
+      rating: 4.7,
+      img: img47,
+      category: "Tecnología",
+      subcategory: "Impresión",
+    },
+    {
+      id: 48,
+      name: "HP DeskJet 3775",
+      price: 400000,
+      discountPrice: 380000,
+      rating: 4.5,
+      img: img48,
+      category: "Tecnología",
+      subcategory: "Impresión",
+    },
+    {
+      id: 49,
+      name: "Ricoh SP 230SFNw",
+      price: 1000000,
+      discountPrice: 950000,
+      rating: 4.6,
+      img: img49,
+      category: "Tecnología",
+      subcategory: "Impresión",
+    },
+    {
+      id: 50,
+      name: "Xerox Phaser 6510",
+      price: 1500000,
+      discountPrice: 1450000,
+      rating: 4.8,
+      img: img50,
+      category: "Tecnología",
+      subcategory: "Impresión",
+    },
+
+    // Cámaras (faltan 5 productos)
+    {
+      id: 51,
+      name: "Nikon D850",
+      price: 10000000,
+      discountPrice: 9800000,
+      rating: 4.9,
+      img: img51,
+      category: "Tecnología",
+      subcategory: "Cámaras",
+    },
+    {
+      id: 52,
+      name: "Fujifilm X-T4",
+      price: 6000000,
+      discountPrice: 5800000,
+      rating: 4.8,
+      img: img52,
+      category: "Tecnología",
+      subcategory: "Cámaras",
+    },
+    {
+      id: 53,
+      name: "Leica Q2",
+      price: 15000000,
+      discountPrice: 14500000,
+      rating: 4.9,
+      img: img53,
+      category: "Tecnología",
+      subcategory: "Cámaras",
+    },
+    {
+      id: 54,
+      name: "Olympus OM-D E-M1 Mark III",
+      price: 4500000,
+      discountPrice: 4300000,
+      rating: 4.7,
+      img: img54,
+      category: "Tecnología",
+      subcategory: "Cámaras",
+    },
+    {
+      id: 55,
+      name: "Pentax K-1 Mark II",
+      price: 5000000,
+      discountPrice: 4800000,
+      rating: 4.6,
+      img: img55,
+      category: "Tecnología",
+      subcategory: "Cámaras",
+    },
+
+    // Refrigeración (faltan 2 productos)
+    {
+      id: 56,
+      name: "Frigorífico Daewoo",
+      price: 3200000,
+      discountPrice: 3100000,
+      rating: 4.6,
+      img: img56,
+      category: "Electrodomésticos",
+      subcategory: "Refrigeración",
+    },
+    {
+      id: 57,
+      name: "Congelador Whirlpool",
+      price: 2700000,
+      discountPrice: 2600000,
+      rating: 4.7,
+      img: img57,
+      category: "Electrodomésticos",
+      subcategory: "Refrigeración",
+    },
+
+    // Lavadoras/Secadoras (faltan 5 productos)
+    {
+      id: 58,
+      name: "Lavadora LG TurboWash",
+      price: 3000000,
+      discountPrice: 2900000,
+      rating: 4.8,
+      img: img58,
+      category: "Electrodomésticos",
+      subcategory: "Lavadoras-Secadoras",
+    },
+    {
+      id: 59,
+      name: "Secadora Electrolux",
+      price: 2800000,
+      discountPrice: 2700000,
+      rating: 4.7,
+      img: img59,
+      category: "Electrodomésticos",
+      subcategory: "Lavadoras-Secadoras",
+    },
+    {
+      id: 60,
+      name: "Lavadora Samsung AddWash",
+      price: 3500000,
+      discountPrice: 3400000,
+      rating: 4.9,
+      img: img60,
+      category: "Electrodomésticos",
+      subcategory: "Lavadoras-Secadoras",
+    },
+    {
+      id: 61,
+      name: "Secadora Bosch Serie 6",
+      price: 2900000,
+      discountPrice: 2800000,
+      rating: 4.8,
+      img: img61,
+      category: "Electrodomésticos",
+      subcategory: "Lavadoras-Secadoras",
+    },
+    {
+      id: 62,
+      name: "Lavadora Mabe Aqua Saver",
+      price: 2400000,
+      discountPrice: 2300000,
+      rating: 4.6,
+      img: img62,
+      category: "Electrodomésticos",
+      subcategory: "Lavadoras-Secadoras",
+    },
+
+    // Audio (falta 1 producto)
+    {
+      id: 63,
+      name: "JBL Flip 6",
+      price: 500000,
+      discountPrice: 470000,
+      rating: 4.8,
+      img: img63,
+      category: "Tecnología",
+      subcategory: "Audio",
+    },
+
+    // Tabletas (faltan 3 productos)
+    {
+      id: 64,
+      name: "iPad Pro 12.9 M2",
+      price: 6000000,
+      discountPrice: 5800000,
+      rating: 4.9,
+      img: img64,
+      category: "Celulares",
+      subcategory: "Tabletas",
+    },
+    {
+      id: 65,
+      name: "Samsung Galaxy Tab S7",
+      price: 2000000,
+      discountPrice: 1900000,
+      rating: 4.8,
+      img: img65,
+      category: "Celulares",
+      subcategory: "Tabletas",
+    },
+    {
+      id: 66,
+      name: "Microsoft Surface Pro 7",
+      price: 4000000,
+      discountPrice: 3900000,
+      rating: 4.7,
+      img: img66,
+      category: "Celulares",
+      subcategory: "Tabletas",
+    },
+
+    // Smartwatch (faltan 3 productos)
+    {
+      id: 67,
+      name: "Apple Watch Series 7",
+      price: 1800000,
+      discountPrice: 1700000,
+      rating: 4.9,
+      img: img67,
+      category: "Celulares",
+      subcategory: "Smartwatch",
+    },
+    {
+      id: 68,
+      name: "Samsung Galaxy Watch 4",
+      price: 1200000,
+      discountPrice: 1150000,
+      rating: 4.8,
+      img: img68,
+      category: "Celulares",
+      subcategory: "Smartwatch",
+    },
+    {
+      id: 69,
+      name: "Garmin Fenix 6",
+      price: 2000000,
+      discountPrice: 1900000,
+      rating: 4.7,
+      img: img69,
+      category: "Celulares",
+      subcategory: "Smartwatch",
+    },
+
+    // Sala (faltan 2 productos)
+    {
+      id: 70,
+      name: "Sofá Modular de Cuero",
+      price: 4000000,
+      discountPrice: 3900000,
+      rating: 4.8,
+      img: img70,
+      category: "Hogar",
+      subcategory: "Salas",
+    },
+    {
+      id: 71,
+      name: "Sillón Relax Eléctrico",
+      price: 1800000,
+      discountPrice: 1700000,
+      rating: 4.7,
+      img: img71,
+      category: "Hogar",
+      subcategory: "Salas",
+    },
+
+    // Comedor (faltan 3 productos)
+    {
+      id: 72,
+      name: "Mesa de Comedor Extensible",
+      price: 2400000,
+      discountPrice: 2300000,
+      rating: 4.6,
+      img: img72,
+      category: "Hogar",
+      subcategory: "Comedor",
+    },
+    {
+      id: 73,
+      name: "Juego de Sillas Minimalistas",
+      price: 1500000,
+      discountPrice: 1400000,
+      rating: 4.8,
+      img: img73,
+      category: "Hogar",
+      subcategory: "Comedor",
+    },
+    {
+      id: 74,
+      name: "Bufet Vintage",
+      price: 2000000,
+      discountPrice: 1900000,
+      rating: 4.7,
+      img: img74,
+      category: "Hogar",
+      subcategory: "Comedor",
+    },
+
+    // Cocina (faltan 2 productos)
+    {
+      id: 75,
+      name: "Juego de Ollas Antiadherentes",
+      price: 800000,
+      discountPrice: 750000,
+      rating: 4.6,
+      img: img75,
+      category: "Hogar",
+      subcategory: "Cocina",
+    },
+    {
+      id: 76,
+      name: "Batidora de Pie KitchenAid",
+      price: 1300000,
+      discountPrice: 1250000,
+      rating: 4.9,
+      img: img76,
+      category: "Hogar",
+      subcategory: "Cocina",
+    },
+
+    // Baño (faltan 2 productos)
+    {
+      id: 77,
+      name: "Toallero Eléctrico",
+      price: 600000,
+      discountPrice: 580000,
+      rating: 4.7,
+      img: img77,
+      category: "Hogar",
+      subcategory: "Baño",
+    },
+    {
+      id: 78,
+      name: "Juego de Toallas de Lujo Utopia",
+      price: 400000,
+      discountPrice: 380000,
+      rating: 4.8,
+      img: img78,
+      category: "Hogar",
+      subcategory: "Baño",
+    },
+    {
+      id: 79,
+      name: "Minisplit Olimpo",
+      price: 500000,
+      discountPrice: 470000,
+      rating: 4.7,
+      img: img79,
+      category: "Electrodomésticos",
+      subcategory: "Climatización",
+    },
+    {
+      id: 80,
+      name: "iPhone 16 Pro Max",
+      price: 7500000,
+      discountPrice: 7200000,
+      rating: 5.0,
+      img: img80,
+      category: "Celulares",
+      subcategory: "Celulares",
     }
-  ];
-  
+];
