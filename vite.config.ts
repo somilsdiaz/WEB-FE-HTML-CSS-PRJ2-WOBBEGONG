436e503881f827011e436de4548a60d7b355120f
Val-jimenez <vjimeneza@uninorte.edu.co>
2024-09-24 18:43:24 -0500

    feat(HU2016):Adding HU2017 branch and start migrating to render

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: " https://wobbegong-uninorte.github.io/WEB-FE-HTML-CSS-PRJ2-WOBBEGONG/",
 })
