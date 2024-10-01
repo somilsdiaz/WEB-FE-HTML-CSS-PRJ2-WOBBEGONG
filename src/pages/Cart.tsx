2b89bea0789ad3dfc97f65290c19890d040b4a16
somilsdiaz <somil.sandoval@icloud.com>
2024-10-01 08:12:15 -0500

    feat(UH2026): 1. Added the cart route to the routes/routes.tsx file, 2. Added the header and footer components, 3. Added the breadcrumb

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,12 @@
+
+import MainLayout from "../layouts/MainLayout";
+
+const PLP = () => {
+    return (
+        <MainLayout>
+            <h2>Pronto</h2>
+        </MainLayout>
+    );
+};
+
+export default PLP;
